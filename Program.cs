5b724603892f5050e190fc8f9e9b3c3b1b5d4a07
JakubKoci1 <146720418+JakubKoci1@users.noreply.github.com>
2024-09-09 09:40:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("zadejte cislo mezi 1 a 10");
+int value;
+while (int.TryParse(Console.ReadLine(), out value) &&  value < 0 || value > 11)
+{
+    
+
+    Console.WriteLine("to není ono zkus to znova");
+    
+    
+}
+Console.WriteLine( value);
